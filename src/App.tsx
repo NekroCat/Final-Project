87b096730e64bffdafe7f2a5b30b0f090cc159e2
Mshkopas <Mshkopas@udel.edu>
2025-04-29 11:40:58 -0400

    Animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect} from 'react';
 import './App.css';
 import { Button, Tooltip, Form, OverlayTrigger } from 'react-bootstrap';
 import { List } from 'react-bootstrap-icons';
-import logo from './logo.svg';
 import BasicQuestions from './components/BasicQuestions';
 import DetailedQuestions from './components/DetailedQuestions';
 import { Route, Routes, useNavigate } from 'react-router-dom';
@@ -38,16 +37,16 @@ if (prevKey !== null) {
 function HomePage() {
   const navigate = useNavigate();
   const [key, setKey] = useState<string>(keyData); //for api key input
-  const storedThemeName = localStorage.getItem('SELECTED_THEME') as 'dark' | 'light' | null;
-  const [theme, setTheme] = useState(storedThemeName ? themes[storedThemeName] : themes.dark);  useEffect(() => {
+  const [theme, setTheme] = useState(themes.dark);
+  useEffect(() => {
     document.body.style.backgroundColor = theme.background;
     document.body.style.color = theme.text;
+    const videoElement = document.querySelector('.background-video') as HTMLVideoElement;
+    if (videoElement) {
+      videoElement.play();
+    }
   }, [theme]);
   
-  function handleSetTheme(themeName: 'dark' | 'light') {
-    setTheme(themes[themeName]);
-    localStorage.setItem('SELECTED_THEME', themeName);
-  }
   
   //sets the local storage item to the api key the user inputed
   function handleSubmit() {
@@ -61,54 +60,59 @@ function HomePage() {
   }
 
   return (
-      <div className="App" style={{ backgroundColor: theme.background, color: theme.text }}>
-        <header className="header" style={{ background: theme.headerFooter, color: theme.text }}>
-          <div className="menu-icon">
-            <List size={30} />
-          </div>
-          <div style={{ marginTop: '20px' }}>
-            <Button onClick={() => handleSetTheme('dark')} variant="dark" style={{ marginRight: '10px' }}>Dark Theme</Button>
-            <Button onClick={() => handleSetTheme('light')} variant="secondary" style={{ marginRight: '10px' }}>Light Theme</Button>
-          </div>
-          <h1 className="website-title">Career Pathway - Choose Your Career With Us</h1>
-          <Button variant="outline-light" className="return-button" style={{backgroundColor: theme.button, color: theme.text }}>Return to Main Page</Button>
-        </header>
-        
-        <div className="content" style={{paddingTop: '100px'}}>
-          <h2>Select Your Quiz Type</h2>
-          <div className="button-group">
-            <OverlayTrigger
-              placement="right"
-              overlay={<Tooltip id="tooltip-basic" style={{color:theme.buttonHover}}>A simple quiz with general career-related questions.</Tooltip>}
-            >
-              <Button className="quiz-button" variant="Basic" onClick={() => navigate('/basic-questions')} style={{backgroundColor: theme.button, color: theme.text }}>Basic Questions ❓</Button>
-            </OverlayTrigger>
-            
-            <OverlayTrigger
-              placement="right"
-              overlay={<Tooltip id="tooltip-advanced">A more in-depth quiz with detailed questions.</Tooltip>}
-            >
-              <Button className="quiz-button" variant="Advanced" onClick={() => navigate('/detailed-questions')} style={{backgroundColor: theme.button, color: theme.text }} >Detailed Questions ❓</Button>
-            </OverlayTrigger>
-          </div>
-        </div>
+    <div className="App" style={{color: theme.text }}>
+      {theme === themes.dark && (
+        <video autoPlay loop muted playsInline className="background-video">
+          <source src="./dark_video (video-converter.com).mp4" type="video/mp4"/>
 
-        <div className="logo">
-          <img src={logo} alt="Career Pathway Logo" className="logo" />
+          Your browser does not support the video tag.
+        </video>
+      )}
+      <header className="header" style={{ background: theme.headerFooter, color: theme.text }}>
+        <div className="menu-icon">
+          <List size={30} />
         </div>
-        
-        <footer className="footer">
-          <p>Built by Maksym Shkopas, Marcos Diaz Vazquez, Dhruv Patel</p>
-        </footer>
+        <div style={{ marginTop: '20px' }}>
+          <Button onClick={() => setTheme(themes.dark)} variant="dark" style={{ marginRight: '10px' }}>Dark Theme</Button>
+          <Button onClick={() => setTheme(themes.light)} variant="secondary" style={{ marginRight: '10px' }}>Light Theme</Button>
+        </div>
+        <h1 className="website-title">Career Pathway - Choose Your Career With Us</h1>
+        <Button variant="outline-light" className="return-button" style={{ backgroundColor: theme.button, color: theme.text }}>Return to Main Page</Button>
+      </header>
+  
+      <div className="content" style={{ paddingTop: '100px' }}>
+        <h2>Select Your Quiz Type</h2>
+        <div className="button-group">
+          <OverlayTrigger
+            placement="right"
+            overlay={<Tooltip id="tooltip-basic" style={{ color: theme.buttonHover }}>A simple quiz with general career-related questions.</Tooltip>}
+          >
+            <Button className="quiz-button" variant="Basic" onClick={() => navigate('/basic-questions')} style={{ backgroundColor: theme.button, color: theme.text }}>Basic Questions ❓</Button>
+          </OverlayTrigger>
+  
+          <OverlayTrigger
+            placement="right"
+            overlay={<Tooltip id="tooltip-advanced">A more in-depth quiz with detailed questions.</Tooltip>}
+          >
+            <Button className="quiz-button" variant="Advanced" onClick={() => navigate('/detailed-questions')} style={{ backgroundColor: theme.button, color: theme.text }}>Detailed Questions ❓</Button>
+          </OverlayTrigger>
+        </div>
+      </div>
+  
+      <div className='form'> 
         <Form>
-        <Form.Label>API Key:</Form.Label>
-        <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
-        <br></br>
-        <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
+          <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
+          <br />
+          <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
         </Form>
       </div>
-      
+  
+      <footer className="footer">
+        <p>Built by Maksym Shkopas, Marcos Diaz Vazquez, Dhruv Patel</p>
+      </footer>
+    </div>
   );
+  
 }
 
 function App() {
